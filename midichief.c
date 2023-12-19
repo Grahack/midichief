2af0daf8b388680ce4ab89d90f3fcfc406f85ba0
Christophe Gragnic <cgragnic@netc.fr>
2023-12-19 17:01:30 +0100

    Better seq opening (duplex + names + prints)

--- midichief.c
+++ midichief.c
@@ -3,20 +3,30 @@
 #include <alsa/asoundlib.h>
 
 static snd_seq_t *seq_handle;
+static int client_id;
 static int in_port;
+static int out_port;
 
 #define CHK(stmt, msg) if((stmt) < 0) {puts("ERROR: "#msg); exit(1);}
 void midi_open(void)
 {
-    CHK(snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_INPUT, 0),
+    CHK(snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_DUPLEX, 0),
             "Could not open sequencer");
 
-    CHK(snd_seq_set_client_name(seq_handle, "Midi Listener"),
+    CHK(snd_seq_set_client_name(seq_handle, "Midi Chief ALSA client"),
             "Could not set client name");
+    CHK(client_id = snd_seq_client_id(seq_handle),
+            "Could not set client id");
     CHK(in_port = snd_seq_create_simple_port(seq_handle, "listen:in",
                 SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
                 SND_SEQ_PORT_TYPE_APPLICATION),
-            "Could not open port");
+            "Could not open in port");
+    CHK(out_port = snd_seq_create_simple_port(seq_handle, "emit:out",
+                SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_SUBS_READ,
+                SND_SEQ_PORT_TYPE_APPLICATION),
+            "Could not open out port");
+    printf("Starting with id=%d, in_port=%d, out_port=%d\n",
+            client_id, in_port, out_port);
 }
 
 snd_seq_event_t *midi_read(void)
