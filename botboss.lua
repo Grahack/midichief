4c5ef74d216824ec595dd8ae372ad68b9fce888c
Christophe Gragnic <cgragnic@netc.fr>
2023-12-25 09:10:18 +0100

    Additions to botboss.lua

--- botboss.lua
+++ botboss.lua
@@ -1,4 +1,11 @@
 print("BotBoss Lua definitions")
-function on_note_on(chan, note, vel)
-    print("on_note_on code")
+
+function on_note_on(chan, note, velo)
+    if chan==9 then print("chan 9") end
+    print("on_note_on code", chan, note, velo)
+end
+
+function on_note_off(chan, note, velo)
+    if chan==9 then print("chan 9") end
+    print("on_note_off code", chan, note, velo)
 end
