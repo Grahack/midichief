e720b32a52aefe88622405da3a692f3c299991de
Christophe Gragnic <cgragnic@netc.fr>
2023-12-25 11:10:49 +0100

    Filter chan 9 and note 69 from Lua

--- botboss.lua
+++ botboss.lua
@@ -1,11 +1,17 @@
 print("BotBoss Lua definitions")
 
 function on_note_on(chan, note, velo)
-    if chan==9 then print("chan 9") end
-    print("on_note_on code", chan, note, velo)
+    if chan==9 and note == 69 then
+        print("ON chan 9 and note 69")
+    else
+        note_on(chan, note, velo);
+    end
 end
 
 function on_note_off(chan, note, velo)
-    if chan==9 then print("chan 9") end
-    print("on_note_off code", chan, note, velo)
+    if chan==9 and note == 69 then
+        print("OFF chan 9 and note 69")
+    else
+        note_off(chan, note, velo);
+    end
 end
