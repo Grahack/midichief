7e4e0b804257b8e98cef9643b5a650bc0749db90
Christophe Gragnic <cgragnic@netc.fr>
2023-12-29 22:40:02 +0100

    Add example setup.sh file + ignore midichief.log

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+BB_DIR=/home/chri/botboss
+MC_DIR=$BB_DIR/midichief
+LOG=$MC_DIR/midichief.log
+
+log() {
+    echo $1 >> $LOG 2>&1
+}
+
+connect_ALSA() {
+    log "Trying to connect $1 to $2"
+    log "  at $(date +%T)"
+    if aconnect "$1" "$2"; then
+        log "Connected  at $(date +%T)!"
+    else
+        sleep 1
+        connect_ALSA "$1" "$2"
+    fi
+}
+
+log "Setting up at $(date +%T)"
+stdbuf -oL $MC_DIR/midichief $MC_DIR/botboss.lua >> $LOG 2>&1 &
+connect_ALSA "Arturia":0 "MIDI Chief":0
+connect_ALSA "EV-10":0 "MIDI Chief":0
+connect_ALSA "MIDI Chief":1 "EV-10":0
+$BB_DIR/divs-midi-utilities/bin/sendmidi --out "MIDI Chief ALSA client:listen:in" --program-change 15 127
+log "Ready at $(date +%T)"
