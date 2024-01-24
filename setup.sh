29a4b87a60f07a46994ef93bceef95a6981b4fd8
Christophe Gragnic <cgragnic@netc.fr>
2024-01-24 19:57:55 +0000

    Use Fluidsynth

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ BB_DIR=/home/chri/botboss
 MC_DIR=$BB_DIR/midichief
 SM=$BB_DIR/divs-midi-utilities/bin/sendmidi
 LOG=$MC_DIR/midichief.log
+FONT=/usr/share/sounds/sf2/FluidR3_GM.sf2
 
 log() {
     echo $1 >> $LOG 2>&1
@@ -21,10 +22,15 @@ connect_ALSA() {
 
 log "Setting up at $(date +%T)"
 stdbuf -oL $MC_DIR/midichief $MC_DIR/botboss.lua >> $LOG 2>&1 &
+fluidsynth -i --server --audio-driver=alsa -o audio.alsa.device=hw:2 $FONT >> $LOG 2>&1 &
 connect_ALSA "Launchkey":0 "MIDI Chief":0
 connect_ALSA "Launchkey":1 "MIDI Chief":0
 connect_ALSA "MIDI Chief":1 "Launchkey":1
 connect_ALSA "UM-1":0 "MIDI Chief":0
 connect_ALSA "MIDI Chief":1 "UM-1":0
+connect_ALSA "MIDI Chief":1 "FLUID Synth":0
+$SM --out "FLUID Synth" --control-change 1 7 0    # silence chan 1(2)
+$SM --out "FLUID Synth" --control-change 9 7 127  # boost the drums
+# alert MIDI Chief that everything is OK
 $SM --out "MIDI Chief ALSA client:listen:in" --program-change 15 127
 log "Ready at $(date +%T)"
