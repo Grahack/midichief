70adbf1f74fef13f3e4afe2f30a539389aa34312
Christophe Gragnic <cgragnic@netc.fr>
2024-03-01 11:02:53 +0000

    Less gain of Fluidsynth

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ check_running() {
 
 log "Setting up at $(date +%T)"
 stdbuf -oL $MC_DIR/midichief $MC_DIR/botboss.lua >> $LOG 2>&1 &
-fluidsynth -i --server --gain 10 --audio-driver=alsa \
+fluidsynth -i --server --gain 5 --audio-driver=alsa \
            -o audio.alsa.device=hw:$NUM_SOUNDCARD \
            $FONT >> $LOG 2>&1 &
 connect_ALSA "Launchkey":0 "MIDI Chief":0
